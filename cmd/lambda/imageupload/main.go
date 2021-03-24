8bb468ab2a2752026abd90c064ed446f479301fe
keitakn <keita.koga.work@gmail.com>
2021-03-24 22:54:47 +0900

    :sparkles: Base64形式の画像データを受け取り、S3に保存する仮実装コードを追加

--- cmd/lambda/imageupload/main.go
+++ cmd/lambda/imageupload/main.go
@@ -1,15 +1,61 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
 	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	"github.com/aws/aws-sdk-go/aws"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/google/uuid"
 )
+
+var uploader *s3manager.Uploader
+
+//nolint:gochecknoinits
+func init() {
+	region := os.Getenv("REGION")
+
+	sess, err := createSession(region)
+	if err != nil {
+		// TODO ここでエラーが発生した場合、致命的な問題が起きているのでちゃんとしたログを出すように改修する
+		log.Fatalln(err)
+	}
+
+	uploader = s3manager.NewUploader(sess)
+}
+
+type RequestBody struct {
+	Image string `json:"image"`
+}
 
 type ResponseOkBody struct {
 	Message string `json:"message"`
+}
+
+type ResponseErrorBody struct {
+	Message string `json:"message"`
+}
+
+func createSession(region string) (*session.Session, error) {
+	sess, err := session.NewSession(&aws.Config{
+		S3ForcePathStyle: aws.Bool(true),
+		Region:           aws.String(region),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return sess, nil
 }
 
 func createApiGatewayV2Response(statusCode int, resBodyJson []byte) events.APIGatewayV2HTTPResponse {
@@ -26,6 +72,75 @@ func createApiGatewayV2Response(statusCode int, resBodyJson []byte) events.APIGa
 }
 
 func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	var reqBody RequestBody
+	if err := json.Unmarshal([]byte(req.Body), &reqBody); err != nil {
+		resBody := &ResponseErrorBody{Message: "Bad Request"}
+		resBodyJson, _ := json.Marshal(resBody)
+
+		statusCode := 400
+
+		res := events.APIGatewayV2HTTPResponse{
+			StatusCode: statusCode,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+			Body:            string(resBodyJson),
+			IsBase64Encoded: false,
+		}
+
+		return res, err
+	}
+
+	decodeImg, err := base64.StdEncoding.DecodeString(reqBody.Image)
+
+	uid, err := uuid.NewRandom()
+	if err != nil {
+		resBody := &ResponseErrorBody{Message: "Failed Generate UUID"}
+		resBodyJson, _ := json.Marshal(resBody)
+
+		statusCode := 500
+
+		res := events.APIGatewayV2HTTPResponse{
+			StatusCode: statusCode,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+			Body:            string(resBodyJson),
+			IsBase64Encoded: false,
+		}
+
+		return res, err
+	}
+
+	buffer := new(bytes.Buffer)
+	buffer.Write(decodeImg)
+
+	uploadKey := "tmp/" + uid.String() + ".jpg"
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket:      aws.String(os.Getenv("TRIGGER_BUCKET_NAME")),
+		Body:        buffer,
+		ContentType: aws.String("image/jpeg"),
+		Key:         aws.String(uploadKey),
+	})
+
+	if err != nil {
+		resBody := &ResponseErrorBody{Message: "Failed Upload To S3"}
+		resBodyJson, _ := json.Marshal(resBody)
+
+		statusCode := 500
+
+		res := events.APIGatewayV2HTTPResponse{
+			StatusCode: statusCode,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+			Body:            string(resBodyJson),
+			IsBase64Encoded: false,
+		}
+
+		return res, err
+	}
+
 	resBody := &ResponseOkBody{Message: "Hello Amazon Rekognition🐱"}
 	resBodyJson, _ := json.Marshal(resBody)
 

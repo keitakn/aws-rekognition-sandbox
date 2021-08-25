8afac417dc8210d438f826dee4c6061967286f97
keitakn <keita.koga.work@gmail.com>
2021-08-25 13:43:45 +0900

    :white_check_mark: #5 ねこ画像と判定されないケースのテストケースを追加、Linterの指摘を修正

--- test/scenario/judgeifcatimagetest/judge_if_cat_image_scenario_test.go
+++ test/scenario/judgeifcatimagetest/judge_if_cat_image_scenario_test.go
@@ -22,15 +22,16 @@ func TestMain(m *testing.M) {
 
 //nolint:funlen
 func TestHandler(t *testing.T) {
-	t.Run("Successful judged to be a cat image", func(t *testing.T) {
+	const expectedTriggerBucketName = "trigger-bucket"
+	const expectedTargetS3ObjectVersionId = "AAAAA.1234567890123456789abcdefg"
+	const catLabelName = "Cat"
+
+	t.Run("judged to be a cat image", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
 
 		mockRekognitionClient := mock.NewMockRekognitionClient(ctrl)
-
-		expectedTriggerBucketName := "trigger-bucket"
 		expectedTargetS3ObjectKey := "tmp/sample-cat-image.jpg"
-		expectedTargetS3ObjectVersionId := "AAAAA.1234567890123456789abcdefg"
 
 		s3Object := &types.S3Object{
 			Bucket:  aws.String(expectedTriggerBucketName),
@@ -54,9 +55,9 @@ func TestHandler(t *testing.T) {
 		}
 
 		confidenceExpected := float32(90.1)
-		expectedFirstLabelName := "Cat"
+		expectedFirstLabelName := catLabelName
 		expectedSecondLabelName := "ChinchillaSilver"
-		expectedFirstParentsName := "Cat"
+		expectedFirstParentsName := catLabelName
 
 		parents := []types.Parent{
 			{Name: aws.String(expectedFirstParentsName)},
@@ -93,6 +94,140 @@ func TestHandler(t *testing.T) {
 		expected := &application.IsCatImageResponse{
 			IsCatImage:  true,
 			TypesOfCats: []string{expectedSecondLabelName},
+		}
+
+		if reflect.DeepEqual(res, expected) == false {
+			t.Error("\nActually: ", res, "\nExpected: ", expected)
+		}
+	})
+
+	t.Run("judged that it is not a cat image, because the confidence value is low", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mockRekognitionClient := mock.NewMockRekognitionClient(ctrl)
+		expectedTargetS3ObjectKey := "tmp/sample-cat-image.jpg"
+
+		s3Object := &types.S3Object{
+			Bucket:  aws.String(expectedTriggerBucketName),
+			Name:    aws.String(expectedTargetS3ObjectKey),
+			Version: aws.String(expectedTargetS3ObjectVersionId),
+		}
+
+		rekognitionImage := &types.Image{
+			S3Object: s3Object,
+		}
+
+		// 何個までラベルを取得するかの設定、ラベルは信頼度が高い順に並んでいる
+		const maxLabels = int32(10)
+		// 信頼度の閾値、Confidenceがここで設定した値未満の場合、そのラベルはレスポンスに含まれない
+		const minConfidence = float32(85)
+
+		detectLabelsInput := &rekognition.DetectLabelsInput{
+			Image:         rekognitionImage,
+			MaxLabels:     aws.Int32(maxLabels),
+			MinConfidence: aws.Float32(minConfidence),
+		}
+
+		confidenceExpected := float32(90.0)
+		expectedFirstLabelName := catLabelName
+
+		labels := []types.Label{
+			{Confidence: aws.Float32(confidenceExpected), Name: aws.String(expectedFirstLabelName)},
+		}
+
+		detectLabelsOutput := &rekognition.DetectLabelsOutput{
+			Labels: labels,
+		}
+
+		ctx := context.Background()
+
+		mockRekognitionClient.EXPECT().DetectLabels(ctx, detectLabelsInput).Return(detectLabelsOutput, nil)
+
+		scenario := application.JudgeIfCatImageScenario{
+			RekognitionClient: mockRekognitionClient,
+		}
+
+		req := &application.JudgeIfCatImageRequest{
+			TargetS3BucketName:      expectedTriggerBucketName,
+			TargetS3ObjectKey:       expectedTargetS3ObjectKey,
+			TargetS3ObjectVersionId: expectedTargetS3ObjectVersionId,
+		}
+
+		res, err := scenario.JudgeIfCatImage(ctx, req)
+		if err != nil {
+			t.Fatal("Failed JudgeIfCatImage", err)
+		}
+
+		expected := &application.IsCatImageResponse{
+			IsCatImage: false,
+		}
+
+		if reflect.DeepEqual(res, expected) == false {
+			t.Error("\nActually: ", res, "\nExpected: ", expected)
+		}
+	})
+
+	t.Run("judged that it is not a cat image, because there is no cat in the image", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mockRekognitionClient := mock.NewMockRekognitionClient(ctrl)
+		expectedTargetS3ObjectKey := "tmp/sample-dog-image.jpg"
+
+		s3Object := &types.S3Object{
+			Bucket:  aws.String(expectedTriggerBucketName),
+			Name:    aws.String(expectedTargetS3ObjectKey),
+			Version: aws.String(expectedTargetS3ObjectVersionId),
+		}
+
+		rekognitionImage := &types.Image{
+			S3Object: s3Object,
+		}
+
+		// 何個までラベルを取得するかの設定、ラベルは信頼度が高い順に並んでいる
+		const maxLabels = int32(10)
+		// 信頼度の閾値、Confidenceがここで設定した値未満の場合、そのラベルはレスポンスに含まれない
+		const minConfidence = float32(85)
+
+		detectLabelsInput := &rekognition.DetectLabelsInput{
+			Image:         rekognitionImage,
+			MaxLabels:     aws.Int32(maxLabels),
+			MinConfidence: aws.Float32(minConfidence),
+		}
+
+		confidenceExpected := float32(99.9)
+		expectedFirstLabelName := "Dog"
+
+		labels := []types.Label{
+			{Confidence: aws.Float32(confidenceExpected), Name: aws.String(expectedFirstLabelName)},
+		}
+
+		detectLabelsOutput := &rekognition.DetectLabelsOutput{
+			Labels: labels,
+		}
+
+		ctx := context.Background()
+
+		mockRekognitionClient.EXPECT().DetectLabels(ctx, detectLabelsInput).Return(detectLabelsOutput, nil)
+
+		scenario := application.JudgeIfCatImageScenario{
+			RekognitionClient: mockRekognitionClient,
+		}
+
+		req := &application.JudgeIfCatImageRequest{
+			TargetS3BucketName:      expectedTriggerBucketName,
+			TargetS3ObjectKey:       expectedTargetS3ObjectKey,
+			TargetS3ObjectVersionId: expectedTargetS3ObjectVersionId,
+		}
+
+		res, err := scenario.JudgeIfCatImage(ctx, req)
+		if err != nil {
+			t.Fatal("Failed JudgeIfCatImage", err)
+		}
+
+		expected := &application.IsCatImageResponse{
+			IsCatImage: false,
 		}
 
 		if reflect.DeepEqual(res, expected) == false {
